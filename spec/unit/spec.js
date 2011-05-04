689e2e41b575445a5aba94a69cbb150e338b8d47
Sylvester Keil <sylvester@keil.or.at>
2011-05-04 23:21:50 +0200

    Initial import.

---
+++ spec/unit/spec.js
@@ -0,0 +1,13 @@
+
+describe 'jQuery-Smoothie Plugin'
+  describe '.smoothie()'
+		it 'should be a function'
+			jQuery.isFunction(jQuery().smoothie).should.be true
+		end
+  end
+
+	describe 'smooth scrolling'
+		before_each
+		end
+	end
+end
